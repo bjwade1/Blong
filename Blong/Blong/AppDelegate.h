a3289f694c4ceecbfc35c9149d3cea0da1b80787
Brandon Wade <brandon.wade3@gmail.com>
2015-10-10 16:13:13 -0500

    Blong background field
    Built the background for the Blong view

---
+++ Blong/Blong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blong
+//
+//  Created by Brandon Wade on 10/7/15.
+//  Copyright © 2015 Brandon Wade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
